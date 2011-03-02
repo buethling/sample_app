ce0ce6be16b94223ca9b24837feaeb1298eff8f0
Chris Lydon <chris.lydon@gmail.com>
2011-03-02 12:47:20 -0500

    Stuff n' things.

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '3.0.0'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+gem 'gravatar_image_tag', '1.0.0.pre2'
 
 group :development do
   gem 'rspec-rails', '2.5.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  get "sessions/new"
+
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
   match '/signup', :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/contact', :to => 'pages#contact'
-    match '/about',   :to => 'pages#about'
-      match '/help',    :to => 'pages#help'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-      root :to => 'pages#home'
+  root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
